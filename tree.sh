1eefd42a31dfef36b296acac9ca4d03233679c53
Lin Yongting <linyongting@gmail.com>
2018-07-23 20:37:44 +0800

    refactor tree.sh to build a more simple object dependency chain

--- tree.sh
+++ tree.sh
@@ -1,21 +1,38 @@
-#!/bin/bash -x
+#!/bin/bash
 
 BUILD_TREE=.build
+
+function path2id()
+{
+	echo "$1" | sed 's/\//_/g'
+}
+
 function build_construct_tree()
 {
-    local cmd="$1"
-    local cmdline="$2"
-    local out=$3
-    local in="$4"
+	local cmd="$1"
+	local cmdline="$2"
+	local out=$3
+	local in="$4"
 
-    echo "cmd=$1 cmdline=$2 out=$3 in=$4" >> log.tree
-    mkdir -p ${BUILD_TREE}/$out
-    touch ${BUILD_TREE}/$out/out
-    echo $cmd > ${BUILD_TREE}/$out/command
-    echo $cmdline > ${BUILD_TREE}/$out/cmdline
-    for i in ${in}
-    do
-        mkdir -p ${BUILD_TREE}/$out/$i
-        touch ${BUILD_TREE}/$out/$i/in
-    done
+	echo "cmd=$1 cmdline=$2 out=$3 in=$4" >> log.tree
+   
+	out_path=$(path2id $out)
+	mkdir -p ${BUILD_TREE}/${out_path}
+	echo $cmd > ${BUILD_TREE}/${out_path}/command
+	echo $cmdline > ${BUILD_TREE}/${out_path}/cmdline
+
+	for i in ${in}
+	do
+		ipath=$(path2id $i)
+
+		if [ -e ${BUILD_TREE}/${out_path}/${ipath} ]; then
+			rm -rf ${BUILD_TREE}/${out_path}/${ipath}
+		fi
+
+		if [ -e ${BUILD_TREE}/${ipath} ]; then
+			mv ${BUILD_TREE}/${ipath} ${BUILD_TREE}/${out_path}/
+		else
+			mkdir -p ${BUILD_TREE}/${out_path}/${ipath}
+		fi
+	done
 }
